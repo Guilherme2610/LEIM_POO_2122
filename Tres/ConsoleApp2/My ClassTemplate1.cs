466a5c711fd3d511444fe9303c9b5c2feebd2f6b
lufer <lufer@ipca.pt>
2021-09-26 18:40:33 +0100

    Inicio do Repositorio

---
+++ Tres/ConsoleApp2/My ClassTemplate1.cs
@@ -0,0 +1,60 @@
+﻿/*
+*	<copyright file="ConsoleApp2.cs" company="IPCA">
+*		Copyright (c) 2021 All Rights Reserved
+*	</copyright>
+* 	<author>lufer</author>
+*   <date>9/24/2021 10:44:27 PM</date>
+*	<description></description>
+**/
+using System;
+
+namespace ConsoleApp2
+{
+    /// <summary>
+    /// Purpose:
+    /// Created by: lufer
+    /// Created on: 9/24/2021 10:44:27 PM
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    public class My_ClassTemplate1
+    {
+        #region Attributes
+        #endregion
+
+        #region Methods
+
+        #region Constructors
+
+        /// <summary>
+        /// The default Constructor.
+        /// </summary>
+        public My_ClassTemplate1()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+
+
+        #region Overrides
+        #endregion
+
+        #region OtherMethods
+        #endregion
+
+        #region Destructor
+        /// <summary>
+        /// The destructor.
+        /// </summary>
+        ~My_ClassTemplate1()
+        {
+        }
+        #endregion
+
+        #endregion
+    }
+}
